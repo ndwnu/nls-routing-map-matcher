ef4c40e7caac57e7af525ca6aae533dcdb6df349
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2021-05-26 14:30:09 +0200

    #26496 add logging

--- src/main/java/nu/ndw/nls/routingmapmatcher/domain/RoutingMapMatcher.java
+++ src/main/java/nu/ndw/nls/routingmapmatcher/domain/RoutingMapMatcher.java
@@ -1,23 +1,20 @@
 package nu.ndw.nls.routingmapmatcher.domain;
 
-import com.google.common.base.Supplier;
+import lombok.extern.slf4j.Slf4j;
 import nu.ndw.nls.routingmapmatcher.domain.model.MapMatchingRequest;
 import nu.ndw.nls.routingmapmatcher.domain.model.MatchStatus;
 import nu.ndw.nls.routingmapmatcher.domain.model.RoutingNetwork;
 import nu.ndw.nls.routingmapmatcher.domain.model.linestring.LineStringLocation;
 import nu.ndw.nls.routingmapmatcher.domain.model.linestring.LineStringMatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+@Slf4j
 public class RoutingMapMatcher {
 
     private final LineStringMapMatcherFactory lineStringMapMatcherFactory;
-
-    private static final Logger logger = LoggerFactory.getLogger(RoutingMapMatcher.class);
 
     public RoutingMapMatcher(LineStringMapMatcherFactory lineStringMapMatcherFactory) {
         this.lineStringMapMatcherFactory = lineStringMapMatcherFactory;
@@ -31,8 +28,10 @@ public class RoutingMapMatcher {
                 .createLineStringMapMatcher(routingNetwork);
         AtomicInteger matched = new AtomicInteger();
         AtomicInteger processed = new AtomicInteger();
+        log.info("Start map matching for " + mapMatchingRequest.getName() + ", count = {}", numLocations);
         return locations
-                .stream().map(lineStringLocation ->
+                .stream()
+                .map(lineStringLocation ->
                         getLineStringMatch(numLocations, lineStringMapMatcher, matched, processed, lineStringLocation)
                 );
     }
@@ -42,12 +41,17 @@ public class RoutingMapMatcher {
                                                       AtomicInteger processed,
                                                       LineStringLocation lineStringLocation) {
         LineStringMatch match = lineStringMapMatcher.match(lineStringLocation);
-        processed.getAndIncrement();
+        processed.incrementAndGet();
         if (MatchStatus.MATCH.equals(match.getStatus())) {
-            matched.getAndIncrement();
+            matched.incrementAndGet();
+            log.info("matched successful {}", matched);
         }
         if ((processed.get() + 1) % 100 == 0) {
-            logger.info("Processed {} of {} total", processed.get() + 1, numLocations);
+            log.info("Processed {} of {} total", processed.get() + 1, numLocations);
+        }
+        if (processed.intValue() == numLocations) {
+            log.info("Done. Processed {} locations, {} successfully matched ({}%)", numLocations, matched.get(),
+                    matched.get() * 10000 / numLocations / 100.0);
         }
         return match;
     }
