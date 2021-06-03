24f497f3fa25fed82eac3aa4883a75e1b320c031
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2021-06-03 14:53:13 +0200

    #26898 unit tests

---
+++ map-matcher-library/src/test/java/nu/ndw/nls/routingmapmatcher/graphhopper/NetworkReaderTest.java
@@ -0,0 +1,110 @@
+package nu.ndw.nls.routingmapmatcher.graphhopper;
+
+import com.graphhopper.coll.LongIntMap;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.PointList;
+import nu.ndw.nls.routingmapmatcher.domain.model.Link;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.LineString;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NetworkReaderTest {
+
+    private static final double LONG_1 = 5.358247;
+    private static final double LAT_1 = 52.161257;
+    private static final double LONG_2 = 5.379687;
+    private static final double LAT_2 = 52.158304;
+    private static final double LONG_3 = 5.379667;
+    private static final double LAT_3 = 52.158280;
+
+    private static final long FROM_NODE_ID = 1;
+    private static final long TO_NODE_ID = 2;
+
+    @Captor
+    private ArgumentCaptor<PointList> pointListArgumentCaptor;
+
+    @Mock
+    private EncodingManager encodingManager;
+    @Mock
+    private GraphHopperStorage ghStorage;
+
+    @Mock
+    private EdgeIteratorState edgeIteratorState;
+
+    @Mock
+    private LineString lineString;
+
+    @Mock
+    private EdgeIteratorState edge;
+
+    @Mock
+    private Link link;
+    @Mock
+    private LongIntMap longIntMap;
+
+    private Coordinate coordinateA1;
+
+    private Coordinate coordinateA2;
+
+    private Coordinate coordinateA3;
+
+
+    private NetworkReader networkReader;
+
+    @BeforeEach
+    void setup() {
+        when(ghStorage.getEncodingManager()).thenReturn(encodingManager);
+        when(ghStorage.edge(anyInt(), anyInt())).thenReturn(edge);
+        when(edge.setDistance(anyDouble())).thenReturn(edge);
+        when(edge.setFlags(any())).thenReturn(edge);
+        when(edge.setWayGeometry(any())).thenReturn(edge);
+        when(link.getFromNodeId()).thenReturn(FROM_NODE_ID);
+        when(link.getToNodeId()).thenReturn(TO_NODE_ID);
+        createCoordinates();
+        final Coordinate[] coordinates = {coordinateA1, coordinateA2, coordinateA3};
+        when(lineString.getCoordinates()).thenReturn(coordinates);
+        when(link.getGeometry()).thenReturn(lineString);
+        final List<Link> links = Collections.singletonList(link);
+        final Supplier<Iterator<Link>> networkSupplier = links::iterator;
+        networkReader = new NetworkReader(ghStorage, networkSupplier, longIntMap);
+    }
+
+    @Test
+    void testReadGraph() {
+        networkReader.readGraph();
+        verify(edge).setWayGeometry(pointListArgumentCaptor.capture());
+        final PointList pointList = pointListArgumentCaptor.getValue();
+        assertThat(pointList.getSize(), is(1));
+        assertThat(pointList.getLon(0), is(LONG_2));
+        assertThat(pointList.getLat(0), is(LAT_2));
+
+    }
+
+    void createCoordinates() {
+        coordinateA1 = new Coordinate(LONG_1, LAT_1);
+        coordinateA2 = new Coordinate(LONG_2, LAT_2);
+        coordinateA3 = new Coordinate(LONG_3, LAT_3);
+    }
+}
