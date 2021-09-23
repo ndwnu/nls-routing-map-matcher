b197f5b4e474e18078a54e7427e86e01990fc1b4
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2021-09-23 07:44:58 +0000

    Merged PR 10654: #31133 Allow use of multiple MapMatcherFactory beans
    - Add SinglePointMapMatcherFactory bean
    - Fix type erasure issue with @ConditionalOnMissingBean
    
    Related work items: #31133

--- map-matcher-spring-boot-autoconfigure/src/main/java/nu/ndw/nls/routingmapmatcher/config/MapMatcherAutoConfiguration.java
+++ map-matcher-spring-boot-autoconfigure/src/main/java/nu/ndw/nls/routingmapmatcher/config/MapMatcherAutoConfiguration.java
@@ -3,8 +3,10 @@ package nu.ndw.nls.routingmapmatcher.config;
 import nu.ndw.nls.routingmapmatcher.domain.LineStringMapMatcher;
 import nu.ndw.nls.routingmapmatcher.domain.MapMatcherFactory;
 import nu.ndw.nls.routingmapmatcher.domain.RoutingMapMatcher;
+import nu.ndw.nls.routingmapmatcher.domain.SinglePointMapMatcher;
 import nu.ndw.nls.routingmapmatcher.domain.StartToEndMapMatcher;
 import nu.ndw.nls.routingmapmatcher.graphhopper.NetworkGraphHopperFactory;
+import nu.ndw.nls.routingmapmatcher.graphhopper.singlepoint.GraphHopperSinglePointMapMatcherFactory;
 import nu.ndw.nls.routingmapmatcher.graphhopper.starttoend.GraphHopperStartToEndMapMatcherFactory;
 import nu.ndw.nls.routingmapmatcher.graphhopper.viterbi.ViterbiLinestringMapMatcherFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -30,7 +32,7 @@ public class MapMatcherAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(value = LineStringMapMatcher.class, parameterizedContainer = MapMatcherFactory.class)
     public MapMatcherFactory<LineStringMapMatcher> lineStringMapMatcherFactory() {
         return new ViterbiLinestringMapMatcherFactory(
                 new NetworkGraphHopperFactory()
@@ -38,7 +40,15 @@ public class MapMatcherAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(value = SinglePointMapMatcher.class, parameterizedContainer = MapMatcherFactory.class)
+    public MapMatcherFactory<SinglePointMapMatcher> singlePointMapMatcherFactory() {
+        return new GraphHopperSinglePointMapMatcherFactory(
+                new NetworkGraphHopperFactory()
+        );
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = StartToEndMapMatcher.class, parameterizedContainer = MapMatcherFactory.class)
     public MapMatcherFactory<StartToEndMapMatcher> startToEndMapMatcherFactory() {
         return new GraphHopperStartToEndMapMatcherFactory(
                 new NetworkGraphHopperFactory()
