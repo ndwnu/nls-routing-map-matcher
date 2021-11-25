6afd937f27bdb9c9265eac413270035b33dcfcc6
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2021-11-25 13:16:52 +0100

    #33329 Added GraphHopperNetworkPathUtils which can be used to build Paths where graph hopper networks for specific network names and versions will be stored

---
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/util/GraphHopperNetworkPathUtils.java
@@ -0,0 +1,22 @@
+package nu.ndw.nls.routingmapmatcher.util;
+
+
+import java.nio.file.Path;
+
+public final class GraphHopperNetworkPathUtils {
+
+    private GraphHopperNetworkPathUtils() {
+    }
+
+    public static String formatNetworkNameAndVersion(String networkNamePrefix, String version) {
+        return networkNamePrefix + version;
+    }
+
+    public static Path formatNormalizedPath(Path rootDirectory, String networkNamePrefixAndVersion) {
+        return rootDirectory.resolve(networkNamePrefixAndVersion).normalize();
+    }
+
+    public static Path formatNormalizedPath(Path rootDirectory, String networkNamePrefix, String version) {
+        return formatNormalizedPath(rootDirectory, formatNetworkNameAndVersion(networkNamePrefix, version));
+    }
+}
