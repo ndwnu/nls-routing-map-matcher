b30834e594edc6cb41f372e51609dee870f2b730
NLS RenovateBot <167181728+ndwlocatieservices@users.noreply.github.com>
2022-11-29 15:20:39 +0000

    Merged PR 18878: Update all non-major dependencies
    ## HUMAN REVIEWER NOTES
    Graphhopper updates were excluded, since they involve a lot of functional change and the target versions are three years old. A separate RenovateBot PR already exists to update to the most recent versions, but this is a major update.
    
    This PR contains the following updates:
    
    | Package | Type | Update | Change |
    |---|---|---|---|
    | [org.apache.maven.plugins:maven-compiler-plugin](https://maven.apache.org/plugins/) | build | minor | `3.8.0` -> `3.10.1` |
    | [org.jacoco:jacoco-maven-plugin](https://www.jacoco.org/jacoco/trunk/doc/maven.html) ([source](https://github.com/jacoco/jacoco)) | build | patch | `0.8.5` -> `0.8.8` |
    | [org.springframework.boot:spring-boot-dependencies](https://spring.io/projects/spring-boot) ([source](https://github.com/spring-projects/spring-boot)) | import | minor | `2.3.7.RELEASE` -> `2.7.6` |
    | [com.google.guava:guava](https://github.com/google/guava) | compile | patch | `23.6-jre` -> `23.6.1-jre` |
    | [org.locationtech.jts:jts-core](https://www.locationtech.org/projects/technology.jts) ([source](https://github.com/locationtech/jts)) | compile | minor | `1.16.1` -> `1.19.0` |
    | [~~com.graphhopper:graphhopper-map-matching-core~~](https://www.graphhopper.com) ([source](https://github.com/graphhopper/map-matching)) | compile | minor | `0.12.0-3` -> `0.13.0-pre19` |
    | [~~com.graphhopper:graphhopper-core~~](https://www.graphhopper.com) ([source](https://github.com/graphhopper/graphhopper)) | compile | minor | `0.12.0` -> `0.14.0-pre1` |
    | [org.projectlombok:lombok](https://projectlombok.org) ([source](https://github.com/projectlombok/lombok)) |  | patch | `1.18.12` -> `1.18.24` |
    | [org.projectlombok:lombok](https://projectlombok.org) ([source](https://github.com/projectlombok/lombok)) | compile | patch | `1.18.12` -> `1.18.24` |
    
    ---
    
    ### Release Notes
    
    <details>
    <summary>jacoco/jacoco</summary>
    
    (Removed due to character limit)
    
    </details>
    
    ---
    
    ### Configuration
    
    📅 **Schedule**: Branch creation - At any time (no schedule defined), Automerge - At any time (no schedule defined).
    
    🚦 **Automerge**: Disabled by config. Please merge this manually once you are satisfied.
    
    ♻ **Rebasing**: Whenever PR becomes conflicted, or rename PR to start with "rebase!".
    
    👻 **Immortal**: This PR will be recreated if closed unmerged. Get [config help](https://github.com/renovatebot/renovate/discussions) if that's undesired.
    
    ---
    
    This PR has been generated by [Renovate Bot](https://github.com/renovatebot/renovate).
    
    Related work items: #48250

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=1.0.1-RELEASE -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
+mvn versions:set -DnewVersion=1.0.2-RELEASE -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
