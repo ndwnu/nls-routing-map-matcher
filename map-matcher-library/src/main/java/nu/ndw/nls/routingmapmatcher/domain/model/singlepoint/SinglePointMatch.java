e68556514d51928eb6060931d8915d65a7bb0a81
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-01-31 16:57:46 +0100

    #49619 fix review comments

--- map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/singlepoint/SinglePointMatch.java
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/singlepoint/SinglePointMatch.java
@@ -23,7 +23,7 @@ public class SinglePointMatch extends MapMatch {
         private final Set<Integer> downstreamLinkIds;
         private final Point snappedPoint;
         private final double fraction;
-        //Distance from input point to snapped point
+        //Distance in meters from input point to snapped point
         private final double distance;
     }
 
