0bf54f177f62074f9faa26fa4d66b17c954db4b4
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2023-02-20 16:23:22 +0100

    #51147 typo

--- map-matcher-library/src/main/java/com/graphhopper/storage/EdgeIteratorStateReverseExtractor.java
+++ map-matcher-library/src/main/java/com/graphhopper/storage/EdgeIteratorStateReverseExtractor.java
@@ -8,7 +8,7 @@ import com.graphhopper.util.EdgeIteratorState;
  * The geometry direction of the edge iterator wayGeometry does not necessarily reflect the direction of a street or the
  * original encoded geometry direction. It is just the direction of the exploration of the graph. GraphHopper sometimes
  * reverses the geometry direction in the EdgeIteratorState with respect to the original geometry direction. This causes
- * problems with the bearing and fraction calculation as well as the determination weather the travelling direction is
+ * problems with the bearing and fraction calculation as well as the determination whether the travelling direction is
  * reversed with respect to the original geometry direction for bidirectional road segments. To fix this an internal
  * attribute of the edge iterator state is used indicating it has done so or not. Since this attribute is package
  * private the same package structure is used.
