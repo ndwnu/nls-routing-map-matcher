678342891da5ec0446eaee84205fe3a1c003a96d
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2023-02-20 17:00:33 +0100

    #51147 support multilinestring as cutoff geometry

--- map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/graphhopper/model/MatchedQueryResult.java
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/graphhopper/model/MatchedQueryResult.java
@@ -1,11 +1,14 @@
 package nu.ndw.nls.routingmapmatcher.graphhopper.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import nu.ndw.nls.routingmapmatcher.domain.model.singlepoint.BearingRange;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.MultiLineString;
 import org.locationtech.jts.geom.Point;
 
 @Value
@@ -20,5 +23,22 @@ public class MatchedQueryResult {
     EdgeIteratorTravelDirection travelDirection;
     Geometry cutoffGeometry;
     boolean reversed;
+
+    public List<LineString> getCutoffGeometriesAsLineStrings() {
+        if (cutoffGeometry instanceof MultiLineString) {
+            var multiLineString = (MultiLineString) cutoffGeometry;
+            List<LineString> lineStrings = new ArrayList<>();
+            for (int i = 0; i < multiLineString.getNumGeometries(); i++) {
+                lineStrings.add((LineString) multiLineString.getGeometryN(i));
+            }
+            return lineStrings;
+        } else if (cutoffGeometry instanceof LineString) {
+            var lineString = (LineString) cutoffGeometry;
+            return List.of(lineString);
+        } else {
+            throw new IllegalStateException("Only LineString or MultiLineString "
+                    + "geometry types are supported " + cutoffGeometry.getClass().getName());
+        }
+    }
 
 }
