41b67a22ca62176fef3431dd3337c402754fe9df
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-02-23 12:33:18 +0100

    #51500 release

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=1.1.5-BETA -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
+mvn versions:set -DnewVersion=1.1.4-RELEASE -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
