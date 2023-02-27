4112077d75e05539e5a3957b652653cb337b1a9c
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2023-02-27 09:42:49 +0100

    #51364 Removing -RELEASE from version to comply with our standard versioning system. 2.0.0-RELEASE and 2.0.0 are the same release!

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=2.0.0-RELEASE -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
+mvn versions:set -DnewVersion=2.0.0 -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
