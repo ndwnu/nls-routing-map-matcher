e6e9ebcd8390f85b944dd77ae701d0ea30914d31
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-04-07 14:05:46 +0200

    #53879 fix review comment

--- map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/singlepoint/MatchFilter.java
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/singlepoint/MatchFilter.java
@@ -3,6 +3,8 @@ package nu.ndw.nls.routingmapmatcher.domain.model.singlepoint;
 import static java.util.Collections.emptyList;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +13,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum MatchFilter {
     ALL(matches -> matches),
-    FIRST(matches -> matches.isEmpty() ? emptyList() : matches.subList(0, 1));
+    FIRST_EQUAL_DISTANCE(matches -> matches.isEmpty() ? emptyList() : filterOnEqual(matches,
+            () -> matches.get(0).getDistance(),
+            MatchedPoint::getDistance)),
+    FIRST_EQUAL_RELIABILITY(matches -> matches.isEmpty() ? emptyList() : filterOnEqual(matches,
+            () -> matches.get(0).getReliability(),
+            MatchedPoint::getReliability));
+    private static final double ROUNDING_ERROR = 0.1;
 
     private final UnaryOperator<List<MatchedPoint>> filter;
+
+    private static List<MatchedPoint> filterOnEqual(List<MatchedPoint> matches,
+            Supplier<Double> firstValueSupplier, Function<MatchedPoint, Double> comparisonProvider) {
+        double firstValue = firstValueSupplier.get() + ROUNDING_ERROR;
+        return matches.stream().filter(matchedPoint -> comparisonProvider.apply(matchedPoint) < firstValue)
+                .toList();
+    }
 }
