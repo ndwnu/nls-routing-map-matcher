f55a111f75a318c3c762b7b2a7d862a29bbd4e45
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2023-04-24 14:17:58 +0200

    #54032 Setting version to 6.0.0

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=6.0.0-SNAPSHOT -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent
+mvn versions:set -DnewVersion=6.0.0 -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent -DgenerateBackupPoms=false
