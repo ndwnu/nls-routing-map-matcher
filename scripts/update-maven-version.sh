9a4922daf106ab4924cd5d9b7321b7d4e159e541
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-09-14 10:58:06 +0200

    #60210 Bump version

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,1 +1,1 @@
-mvn versions:set -DnewVersion=7.1.2 -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=7.2.0 -DgroupId=nu.ndw.nls -DartifactId=routing-map-matcher-parent -DgenerateBackupPoms=false
