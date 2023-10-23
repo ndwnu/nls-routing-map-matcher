0b7e8ac0fb052a43508b3dcb87cad21215d1cfd4
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-10-23 18:07:17 +0200

    #61854 Bump version

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -3,11 +3,11 @@ options=("Major" "Minor")
 select selected in "${options[@]}"; do
   case $selected in
   "Major")
-    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT -DgenerateBackupPoms=false
     break
     ;;
   "Minor")
-    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT -DgenerateBackupPoms=false
     break
     ;;
   *) echo "invalid option $REPLY" ;;
