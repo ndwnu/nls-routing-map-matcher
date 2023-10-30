2205f6c0a6c0581c4f074d9414c1b93b23b51d5e
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2023-10-30 10:16:18 +0100

    #62079 fix sonar

--- map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/accessibility/VehicleProperties.java
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/accessibility/VehicleProperties.java
@@ -3,15 +3,8 @@ package nu.ndw.nls.routingmapmatcher.domain.model.accessibility;
 import lombok.Builder;
 
 @Builder
-public record VehicleProperties(Double length,
-                                Double width,
-                                Double height,
-                                Double weight,
-                                Double axleLoad,
-                                Boolean hgvAccessForbidden,
-                                Boolean carAccessForbidden,
-                                Boolean autoBusAccessForbidden,
-                                Boolean trailerAccessForbidden
-) {
+public record VehicleProperties(Double length, Double width, Double height, Double weight, Double axleLoad,
+                                Boolean hgvAccessForbidden, Boolean carAccessForbidden, Boolean autoBusAccessForbidden,
+                                Boolean trailerAccessForbidden) {
 
 }
