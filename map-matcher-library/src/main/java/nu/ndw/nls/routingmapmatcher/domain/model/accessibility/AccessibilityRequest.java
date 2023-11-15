232dc2c0bc02828d708e87ca12de1f906238b3d3
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-15 14:18:55 +0100

    #62925 change implementation to distance and filter on municipality

--- map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/accessibility/AccessibilityRequest.java
+++ map-matcher-library/src/main/java/nu/ndw/nls/routingmapmatcher/domain/model/accessibility/AccessibilityRequest.java
@@ -4,7 +4,8 @@ import lombok.Builder;
 import org.locationtech.jts.geom.Point;
 
 @Builder
-public record AccessibilityRequest(Point startPoint,int municipalityId, VehicleProperties vehicleProperties) {
+public record AccessibilityRequest(Point startPoint, int municipalityId, double travellingDistanceInMetres,
+                                   VehicleProperties vehicleProperties) {
 
 
 }
