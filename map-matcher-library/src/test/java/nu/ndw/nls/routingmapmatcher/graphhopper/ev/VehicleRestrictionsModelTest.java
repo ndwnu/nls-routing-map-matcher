913ef2cf71b4e0e9c03a63bdc4fdf05e7515b88f
Bas Duineveld <167181728+ndwlocatieservices@users.noreply.github.com>
2023-11-21 16:26:49 +0100

    #63347 Add tags for traffic signs C8, C9 and C22c

--- map-matcher-library/src/test/java/nu/ndw/nls/routingmapmatcher/graphhopper/ev/VehicleRestrictionsModelTest.java
+++ map-matcher-library/src/test/java/nu/ndw/nls/routingmapmatcher/graphhopper/ev/VehicleRestrictionsModelTest.java
@@ -10,39 +10,44 @@ import org.junit.jupiter.api.Test;
 
 class VehicleRestrictionsModelTest {
 
-    private static final String EXPECTED_FULL_EXPRESSION = "max_axle_load < 10.000000 || "
-            + "max_height < 2.650000 || "
-            + "max_length < 8.230000 || "
-            + "max_weight < 26.500000 || "
+    private static final String EXPECTED_FULL_EXPRESSION = "max_length < 8.230000 || "
             + "max_width < 2.550000 || "
+            + "max_height < 2.650000 || "
+            + "max_axle_load < 10.000000 || "
+            + "max_weight < 26.500000 || "
+            + "car_access_forbidden == true || "
             + "hgv_access_forbidden == true || "
-            + "car_access_forbidden == true || "
-            + "auto_bus_access_forbidden == true || "
+            + "bus_access_forbidden == true || "
+            + "hgv_and_bus_access_forbidden == true || "
+            + "tractor_access_forbidden == true || "
+            + "slow_vehicle_access_forbidden == true || "
             + "trailer_access_forbidden == true || "
-            + "hgv_and_auto_bus_access_forbidden == true || "
-            + "motor_bike_access_forbidden == true || "
-            + "motor_vehicle_access_forbidden == true";
-    private static final String EXPECTED_PARTIAL_EXPRESSION = "hgv_access_forbidden == true || "
-            + "car_access_forbidden == true || "
-            + "auto_bus_access_forbidden == true || "
+            + "motorcycle_access_forbidden == true || "
+            + "motor_vehicle_access_forbidden == true || "
+            + "lcv_and_hgv_access_forbidden == true";
+    private static final String EXPECTED_PARTIAL_EXPRESSION = "car_access_forbidden == true || "
+            + "hgv_access_forbidden == true || "
+            + "bus_access_forbidden == true || "
             + "trailer_access_forbidden == true";
 
     @Test
     void vehicleRestrictionsModel_ok() {
-        VehicleProperties vehicleProperties = VehicleProperties
-                .builder()
-                .autoBusAccessForbidden(true)
-                .carAccessForbidden(true)
-                .trailerAccessForbidden(true)
-                .hgvAccessForbidden(true)
-                .motorVehicleAccessForbidden(true)
-                .hgvAndAutoBusAccessForbidden(true)
-                .motorBikeAccessForbidden(true)
-                .axleLoad(10.00)
-                .weight(26.5)
-                .height(2.65)
+        VehicleProperties vehicleProperties = VehicleProperties.builder()
                 .length(8.23)
                 .width(2.55)
+                .height(2.65)
+                .axleLoad(10.00)
+                .weight(26.5)
+                .carAccessForbidden(true)
+                .hgvAccessForbidden(true)
+                .busAccessForbidden(true)
+                .hgvAndBusAccessForbidden(true)
+                .tractorAccessForbidden(true)
+                .slowVehicleAccessForbidden(true)
+                .trailerAccessForbidden(true)
+                .motorcycleAccessForbidden(true)
+                .motorVehicleAccessForbidden(true)
+                .lcvAndHgvAccessForbidden(true)
                 .build();
 
         VehicleRestrictionsModel vehicleRestrictionsModel = new VehicleRestrictionsModel(vehicleProperties);
@@ -54,15 +59,13 @@ class VehicleRestrictionsModelTest {
         assertThat(statement.getCondition()).isEqualTo(EXPECTED_FULL_EXPRESSION);
     }
 
-
     @Test
-    void vehicleRestrictionsModel_with_null_values_ok() {
-        VehicleProperties vehicleProperties = VehicleProperties
-                .builder()
-                .autoBusAccessForbidden(true)
+    void vehicleRestrictionsModel_ok_withNullValues() {
+        VehicleProperties vehicleProperties = VehicleProperties.builder()
                 .carAccessForbidden(true)
-                .trailerAccessForbidden(true)
                 .hgvAccessForbidden(true)
+                .busAccessForbidden(true)
+                .trailerAccessForbidden(true)
                 .build();
 
         VehicleRestrictionsModel vehicleRestrictionsModel = new VehicleRestrictionsModel(vehicleProperties);
@@ -73,8 +76,9 @@ class VehicleRestrictionsModelTest {
         assertThat(statement.getValue()).isEqualTo("0");
         assertThat(statement.getCondition()).isEqualTo(EXPECTED_PARTIAL_EXPRESSION);
     }
+
     @Test
-    void vehicleRestrictionsModel_with_null_properties_ok() {
+    void vehicleRestrictionsModel_ok_withNullProperties() {
         VehicleRestrictionsModel vehicleRestrictionsModel = new VehicleRestrictionsModel(null);
         assertThat(vehicleRestrictionsModel.getPriority()).hasSize(0);
     }
