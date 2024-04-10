6d681065dbc707948124b81a18eb5592f096f686
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-04-10 10:12:55 +0200

    #70374 Add new integration test and update pom.xml configurations
    Two new integration tests were added for ClosestPointService. Dependency setups in map-matcher-geo/pom.xml and map-matcher-library/pom.xml were revised accordingly. Unnecessary configurations and properties removed from map-matcher-geo/pom.xml for brevity and maintainability.

---
+++ map-matcher-geo/src/test/java/nu/ndw/nls/routingmapmatcher/geo/services/ClosestPointServiceIT.java
@@ -0,0 +1,30 @@
+package nu.ndw.nls.routingmapmatcher.geo.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import nu.ndw.nls.routingmapmatcher.geo.model.ProjectionResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.locationtech.jts.geom.Coordinate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestConfig.class})
+class ClosestPointServiceIT {
+    @Autowired
+    private ClosestPointService closestPointService;
+
+    @Test
+    void closestPoint_ok() {
+        ProjectionResult result = closestPointService.closestPoint(
+                List.of(new Coordinate(52.366349, 4.877447), new Coordinate(52.364595, 4.878614)),
+                new Coordinate(52.364848, 4.878124));
+        assertThat(result.distance()).isEqualTo(29.74587257816117);
+        assertThat(result.bearing()).isEqualTo(303.55706380749484);
+        assertThat(result.point()).isEqualTo(new Coordinate(52.364997096061515, 4.878346470864429));
+
+    }
+}
